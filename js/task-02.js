3ec81b37b3f0ba93daa64cc1695d773e0ecfffd6
Osyotr1 <92748388+Osyotr1@users.noreply.github.com>
2022-05-02 17:13:05 +0300

    two tasks done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const listEl = document.querySelector('#ingredients')
+
+const element = ingredients.map(el => {
+  const newEl = document.createElement('li');
+  newEl.textContent = el;
+  listEl.appendChild(newEl);
+  
+})
+// const listRef = document.createElement('li')
+
