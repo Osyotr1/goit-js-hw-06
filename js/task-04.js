ecd16ec1865fd401ddfaea4e255ce4855029839c
Osyotr1 <92748388+Osyotr1@users.noreply.github.com>
2022-05-03 17:00:30 +0300

    task 4 done

---
+++ js/task-04.js
@@ -0,0 +1,16 @@
+const counterValue = document.querySelector('#value');
+console.log(counterValue);
+
+counterValue.innerHTML = 0;
+
+const increaseBtn = document.querySelector('button[data-action="increment"]')
+
+const decreaseBtn = document.querySelector('button[data-action="decrement"]')
+
+increaseBtn.addEventListener('click', () => {
+    counterValue.innerHTML ++;
+});
+
+decreaseBtn.addEventListener('click', () => {
+    counterValue.innerHTML--;
+});
