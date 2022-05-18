73ca48b52ed5fcb0ba5710017e1d55562061e026
Volodymyr Horin <vova.gorin08@gmail.com>
2022-05-18 22:50:14 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const input = document.querySelector('input');
+console.log(input.value);
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+
+let inputValue = Number(input.value);
+
