958017d42a33f137b1898f05e3055b1d756bf27c
Volodymyr Horin <vova.gorin08@gmail.com>
2022-05-18 18:44:55 +0300

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    
+    const userEmail = event.currentTarget.elements.email.value;
+    const userPassword = event.currentTarget.elements.password.value;
+    const userData = {
+        userEmail,
+        userPassword,
+    }
+
+    if (userEmail === '' || userPassword === '') {
+        alert('Заповніть усі поля');
+        form.reset();
+    } else {
+        console.log(userData);
+        form.reset();
+    }
+}
