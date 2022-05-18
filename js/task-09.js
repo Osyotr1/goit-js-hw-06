8b97471490caa08c98c59bfcb3b32acb5d176dd3
Volodymyr Horin <vova.gorin08@gmail.com>
2022-05-18 21:34:41 +0300

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const body = document.querySelector('.widget');
+
+const button = document.querySelector('button.change-color');
+
+const span = document.querySelector('.color');
+
+button.addEventListener('click', changeBodyColor);
+
+function changeBodyColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  span.textContent = getRandomHexColor();
+}
