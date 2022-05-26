9cc2666f3573fa4a4e291d3757d31a2ec566f652
Volodymyr Horin <vova.gorin08@gmail.com>
2022-05-26 14:04:25 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -2,46 +2,44 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const input = document.querySelector('input');
 const createBtn = document.querySelector('[data-create]');
 const destroyBtn = document.querySelector('[data-destroy]');
 const boxesList = document.querySelector('#boxes');
 
-const inputValue = () => Number(input.value);
 
 
+function getInputValue() {
+  const input = document.querySelector('input').value;
+  createBoxes(input);
 
-const createBoxes = () => {
-  
-  const boxes = [];
-  let amount = inputValue();
 
-  for (let i = 1; i <= amount; i++) {
+  function createBoxes(amount) {
+    const boxes = [];
+
+
+    for (let i = 1; i <= amount; i += 1) {
     
+      const box = document.createElement('div');
 
-    const box = document.createElement('div');
-    box.style.backgroundColor = getRandomHexColor();
-    box.style.margin = '20px';
-    box.style.width = `${30 + i * 10}px`;
-    box.style.height = `${30 + i * 10}px`;
-    box.style.border = 'solid 1px black';
-    box.style.borderRadius = '5%';
-    boxes.push(box);
-    
-    
+      box.style.backgroundColor = getRandomHexColor();
+      box.style.margin = '10px';
+      box.style.width = `${20 + i * 10}px`;
+      box.style.height = `${20 + i * 10}px`;
+      box.style.border = 'solid 1px black';
+      box.style.borderRadius = '5%';
+      boxes.push(box);
+      
+    }
+    boxesList.append(...boxes);
   }
-  boxesList.append(...boxes);
-}
+};
 
 const removeBoxes = () => {
   boxesList.innerHTML = '';
 }
 
-
-createBtn.addEventListener('click', createBoxes);
+createBtn.addEventListener('click', getInputValue);
 destroyBtn.addEventListener('click', removeBoxes);
-
-
 
 
 
